cef1174b4f70eb131369fbd16b023a89be25c8ef
nazhot <15216154+nazhot@users.noreply.github.com>
2022-08-17 09:32:31 -0600

    Viewer Updates
    Viewer wasn't really a role before, added some updates that made it a
    role. When viewer joins, they are given the current "READY" status of
    the two player, and no buttons to click. The "READY" statuses update as
    players 1&2 change them, and when the game is being played, the viewer
    is able to see all cards drawn. This is obviously an issue with more
    serious players, since you could just add a new tab to see your
    opponent's cards. Not an issue currently between friends.

--- index.js
+++ index.js
@@ -17,34 +17,25 @@ app.get("/", (req, res) => {
 io.on("connection", (socket) => {
   players.push(socket.id);
   if (players.length === 1) {
-    console.log("emitting player 1");
     io.emit("player1", socket.id);
   } else if (players.length === 2) {
-    console.log("emitting player 2");
     io.emit("player2", socket.id);
   } else {
-    console.log("emitting viewer");
     io.emit("viewer", socket.id);
   }
 
   socket.on("ready-change", (player, readyText) => {
-    console.log("emitting ready-change");
     io.emit("ready-change", player, readyText);
   });
 
   socket.on("start-game", () => {
-    console.log("starting game!");
     io.emit("start-game");
   });
 
   socket.on("draw-card", (isPlayer1) => {
-    console.log((isPlayer1 ? "Player 1 " : "Player 2 ") + " drew a card!");
     let idFile = fs.readFileSync("./public/cards/ids.txt", "utf-8");
     let ids = idFile.split("\n");
     let drawCard = ids[Math.floor(Math.random() * ids.length)];
-    console.log(drawCard);
-    // let files = fs.readdirSync("./public/cardImages/");
-    // let drawCard = files[Math.floor(Math.random() * files.length)];
     io.emit("draw-card", isPlayer1, drawCard);
   });
 
