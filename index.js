2b175eaf2ac8c2631d4c85b83ab68aa0f8f0a9e9
nazhot <15216154+nazhot@users.noreply.github.com>
2022-08-18 13:45:28 -0600

    Added Card/Ban Lists
    Player 1 can now choose the ban/card list to go off of for the
    randomization. Reworked the backend a little bit to be more generic, and
    added code to have it keep track of each players "allowed" cards left
    (Pot of Greed is limited, so 1 is allowed, each player is given a
    starting value of 1 for Pot of Greed. If the card is added, that value
    goes down by 1 to 0, and the program will know to not give Pot of Greed
    to that player again. Rerolling will increment the value by 1). Did some
    refactoring on the client side, to keep the code more concise and
    readable.

--- index.js
+++ index.js
@@ -5,13 +5,39 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 const fs = require("fs");
-const cardFunctions = require("./cardFunctions");
 
 app.use(express.static(__dirname));
 
 let players = [];
-let idFile = fs.readFileSync("./public/cards/ids.txt", "utf-8");
-let ids = idFile.split("\n");
+let cardListFiles = fs.readdirSync("./public/cardLists/");
+let cardListNames = cardListFiles.map((x) => {
+  return x.replace(".txt", "");
+});
+let banListFiles = fs.readdirSync("./public/banLists/");
+let banListNames = banListFiles.map((x) => {
+  return x.replace(".json", "");
+});
+let setListFiles = fs.readdirSync("./public/setLists/");
+let setListNames = setListFiles.map((x) => {
+  return x.replace(".json", "");
+});
+let cardIds;
+let banList;
+let setList;
+
+let playerCardsLeft = {
+  player1: {},
+  player2: {},
+};
+
+function getRandomCard(player) {
+  let drawCard;
+  do {
+    drawCard = cardIds[Math.floor(Math.random() * cardIds.length)];
+  } while (playerCardsLeft[player][drawCard] <= 0);
+  playerCardsLeft[player][drawCard]--;
+  return drawCard;
+}
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
@@ -27,25 +53,45 @@ io.on("connection", (socket) => {
     io.emit("viewer", socket.id);
   }
 
+  socket.on("getLists", () => {
+    io.emit("setLists", cardListNames, banListNames, setListNames);
+  });
+
   socket.on("ready-change", (player, readyText) => {
     io.emit("ready-change", player, readyText);
   });
 
-  socket.on("start-game", () => {
+  socket.on("start-game", (cardListName, banListName, setListName) => {
+    let idFile = fs.readFileSync(
+      "./public/cardLists/" + cardListName + ".txt",
+      "utf-8"
+    );
+    cardIds = idFile.split("\n");
+    let banFile = fs.readFileSync("./public/banLists/" + banListName + ".json");
+    banList = JSON.parse(banFile);
+    for (let i = 0; i < cardIds.length; i++) {
+      let id = cardIds[i];
+      playerCardsLeft.player1[id] = banList[id] ? banList[id] : 3;
+      playerCardsLeft.player2[id] = banList[id] ? banList[id] : 3;
+    }
     io.emit("start-game");
   });
 
   socket.on("draw-card", (isPlayer1) => {
-    let drawCard = cardFunctions.getRandomCard(ids);
+    let player = isPlayer1 ? "player1" : "player2";
+    let drawCard = getRandomCard(player);
     io.emit("draw-card", isPlayer1, drawCard);
   });
 
-  socket.on("reroll", (isPlayer1) => {
-    io.emit("reroll", cardFunctions.getRandomCard(ids), isPlayer1);
+  socket.on("reroll", (isPlayer1, previousCardId) => {
+    let player = isPlayer1 ? "player1" : "player2";
+    playerCardsLeft[player][previousCardId]++;
+    let drawCard = getRandomCard(player);
+    io.emit("reroll", drawCard, isPlayer1);
   });
 
   socket.on("disconnect", () => {
-    players = players.filter((player) => player.id !== socket.id);
+    players = players.filter((player) => player !== socket.id);
     io.emit("disconnected", socket.id, players);
   });
 });
