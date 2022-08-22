0405e2f85f38c26ead3f2a1ebf207de3b7dca33c
nazhot <15216154+nazhot@users.noreply.github.com>
2022-08-22 17:09:14 -0600

    Added data-driven randomness
    Did some webscraping to gain a couple ten thousand user-created decks,
    and made matrices for how cards connect to one another. There is now the
    ability to make the "random" card more or less random, more random means
    those connection weights slowly all diverge together, less random means
    the weights are just themselves. Currently is hard coded in, next I need
    to work on getting the randomness setable by player 1, and textboxes for
    each player to set how many trap/spell/monster cards they want. Will
    need to update the file with card ids to now include card types and
    such.

--- index.js
+++ index.js
@@ -25,6 +25,12 @@ let setListNames = setListFiles.map((x) => {
   return x.replace(".json", "");
 });
 
+let mainDeckConnections = fs.readFileSync("./public/connections/mainDeck-connections.json");
+mainDeckConnections = JSON.parse(mainDeckConnections);
+
+let totalUsages = fs.readFileSync("./public/connections/totalUsages.json");
+totalUsages = JSON.parse(totalUsages);
+
 let cardIds;
 let banList;
 let setList;
@@ -34,13 +40,91 @@ let playerCardsLeft = {
   player2: {},
 };
 
+console.log(playerCardsLeft.player3);
 function getRandomCard(player) {
+  //player: String, "player1" or "player2", whoever is drawing the card
   let drawCard;
   do {
     drawCard = cardIds[Math.floor(Math.random() * cardIds.length)];
   } while (playerCardsLeft[player][drawCard] <= 0);
   playerCardsLeft[player][drawCard]--;
   return drawCard;
+}
+
+function getRandomCardWeighted(player, data, randomFactor){
+  //player:       String, "player1" or "player2"
+  //data:         Object, keys are cardIds, values are weights
+  //randomFactor: float,  0-1, how random to make the card draw, based on the connection map
+
+  let weights = {};
+
+  //add cards from given data set if they're in the card list, and the player can add it
+  for (const [id, value] of Object.entries(data)){
+    if (playerCardsLeft[player][id] > 0){ //if undefined, this is also false
+      weights[id] = value;
+    }
+  }
+
+  //add cards if they're in the card list, not in the current weight object, and player can still add it
+  for (const id of cardIds){
+    if (weights[id] === undefined && playerCardsLeft[player][id] > 0){ //if undefined, this is also false
+      weights[id] = 1;
+    }
+  }
+
+
+  //set each weight to 1 (randomFactor = 1) to itself (randomFactor = 0)
+  let totalWeight = 0;
+  for (const [id, value] of Object.entries(weights)){
+    const divisor = (1 + randomFactor * (parseFloat(value) - 1));
+    weights[id] = parseFloat(value) / divisor;
+    totalWeight += weights[id];
+
+  }
+
+  //normalize array
+  for (const [id, value] of Object.entries(weights)){
+    weights[id] = value / totalWeight;
+  }
+
+  const randomValue = Math.random();
+  let countValue    = 0;
+  let lastId;
+
+  for (const [id, value] of Object.entries(weights)){
+    countValue += value;
+    lastId = id;
+    if (randomValue < countValue){
+      playerCardsLeft[player][id] = playerCardsLeft[player][id] - 1;
+      return id;
+    }
+  }
+
+  playerCardsLeft[player][lastId] = playerCardsLeft[player][lastId] - 1;
+  return lastId;
+
+}
+
+function getRandomCardWeightedByTotalCount(player, randomFactor){
+  return getRandomCardWeighted(player, totalUsages, randomFactor);
+}
+
+function getRandomCardWeightedByLastCard(player, lastCard, randomFactor){
+  //player:       String, "player1" or "player2", whoever is drawing the card
+  //lastCard:     String, id of the last card drawn by player
+  //randomFactor: float,  0-1, how random to make the card draw, based on the connection map
+
+  if (lastCard === null) return getRandomCard(player);
+
+  let connectionsToLastCard = mainDeckConnections[lastCard]; //can I assume lastCard is always in mainDeckConnections?
+  let idArray               = [];
+  let valueArray            = [];
+
+  if (connectionsToLastCard === undefined) {
+    return getRandomCardWeightedByTotalCount(player, randomFactor);
+  }
+
+  return getRandomCardWeighted(player, connectionsToLastCard, randomFactor);
 }
 
 app.get("/", (req, res) => {
@@ -82,9 +166,9 @@ io.on("connection", (socket) => {
     io.emit("start-game");
   });
 
-  socket.on("draw-card", (isPlayer1) => {
+  socket.on("draw-card", (isPlayer1, lastCard) => {
     let player = isPlayer1 ? "player1" : "player2";
-    let drawCard = getRandomCard(player);
+    let drawCard = getRandomCardWeightedByLastCard(player, lastCard, 0);
     io.emit("draw-card", isPlayer1, drawCard);
   });
 
